aa3a84860410991bb3d0bcfae79de480ec7a1ac8
B Cutts <cuttsbrians@gmail.com>
2023-01-15 23:21:08 +0000

    Added script file with pseudo code

---
+++ script.js
@@ -0,0 +1,8 @@
+//Function that returns a random choice from the computer
+//Function that takes computer choice and player choice
+    //compares the two to see who won
+    //returns You Lose or You Win with a description of how the choice
+    //returned that result - NB - only need to return result not log it
+    //player input should be case insensitive
+//Function that plays the game over 5 rounds, each time printing out the 
+//results with console.log
