3283a8e01d7cf3b8b3536c2f1ec90912aed5adc3
B Cutts <cuttsbrians@gmail.com>
2023-01-16 13:26:01 +0000

    Added fuction for computer choice
    Computer choice will randomly return rock, paper or scissors!

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
 //Function that returns a random choice from the computer
+function computerChoice() {
+    //make array with three values, rock paper and scissors
+    const choiceArr = ["Rock", "Paper", "Scissors"];
+
+    //choose a random number between 1 and 3 inclusive
+    let rdmNo = Math.floor(Math.random() * 3) + 1;
+
+    //use this number to choose the item in the array and return it    
+    return choiceArr[(rdmNo - 1)];
+
+
+}
+    
+
 //Function that takes computer choice and player choice
+
     //compares the two to see who won
     //returns You Lose or You Win with a description of how the choice
     //returned that result - NB - only need to return result not log it
