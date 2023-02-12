df5e72b3dc61221bd7d33d260e6c0ab076ef577b
B Cutts <cuttsbrians@gmail.com>
2023-02-12 23:40:22 +0000

    Nearly done, live server error
    I need to merge this branch so that I can use GitHub
    to debug. I can't use the live server to refresh CSS

--- script.js
+++ script.js
@@ -30,16 +30,21 @@ function playRound(computerChoice, playerChoice) {
 function gameEndCheck() {
     return playerScore === 5 || computerScore === 5
 }
+
 const playScorecard = document.getElementById('playerScore');
 const compScorecard = document.getElementById('compScore');
 const resultMsg = document.getElementById('game-message');
 const rockBtn = document.getElementById("Rock");
 const paperBtn = document.getElementById("Paper");
 const scisBtn = document.getElementById("Scissors");
+const gameOverMsg = document.getElementById('gameOverDisp');
+const retryBtn = document.getElementById('retryButton');
+const endingModal = document.getElementById('restartModal');
 
 scisBtn.addEventListener('click', () => gameStart('Scissors'))
 rockBtn.addEventListener('click', () => gameStart('Rock'))
 paperBtn.addEventListener('click', () => gameStart('Paper'))
+retryBtn.addEventListener('click', () => restartGame);
 
 function gameStart(playersChoice) {
     console.log(playersChoice);
@@ -47,6 +52,12 @@ function gameStart(playersChoice) {
         playerChoice = playersChoice;
         playRound(computerChoice, playersChoice);
         roundScoring(computerChoice);
+
+        if (gameEndCheck()){
+            
+            displayGameOver();
+            gameOverMessage();
+        }
 
 }
 
@@ -61,6 +72,23 @@ function roundScoring(computerChoice) {
 
     }else if (roundVictor === 'tie')
         resultMsg.textContent = "Tied the round"
+}
+
+function displayGameOver() {
+    endingModal.classList.add('active')
+}
+
+
+function gameOverMessage () {
+return playerScore > computerScore ? (gameOverMsg.textContent = "You Won!") : (gameOverMsg.textContent = "Oh no! You Lost.");
+}
+
+function restartGame() {
+    playerScore = 0;
+    computerScore = 0;
+    playScorecard.textContent = "Player: 0";
+    compScorecard.textContent = "Computer: 0";
+    endingModal.classList.remove('active');
 }
     
 
