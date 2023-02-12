b0acfc7b4561d2269b0ea378bda7fbaf0f9dd2f4
B Cutts <cuttsbrians@gmail.com>
2023-02-12 20:30:08 +0000

    Plays a game
    Each button will now start a game. There is a game
    message displayed as well as the score for the
    human player and the computer.

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 
-let playerChoice;
-let roundVictor;
-let playerScore;
-let computerScore;
+let playerChoice = '';
+let roundVictor = '';
+let playerScore = 0;
+let computerScore = 0;
 let resultString;
+
+
 
 
 function getComputerChoice() {
@@ -16,14 +18,51 @@ function playRound(computerChoice, playerChoice) {
   
     if(playerChoice === computerChoice)
         roundVictor = 'tie';
-
+    
     if ((playerChoice === "Rock" && computerChoice === "Scissors") || (playerChoice === "Paper" && computerChoice === "Rock") || (playerChoice === "Scissors" && computerChoice === "Paper")) {
         roundVictor = 'player';
+        playerScore++;
     }
+
+    if ((computerChoice === "Rock" && playerChoice === "Scissors") || (computerChoice === "Paper" && playerChoice === "Rock") || (computerChoice === "Scissors" && playerChoice === "Paper")) {
+        roundVictor = 'computer';
+        computerScore++;
+    }
+    
+
         
 }
-    //returns You Lose or You Win with a description of how the choice
-    //returned that result - NB - only need to return result not log it
+const playScorecard = document.getElementById('playerScore');
+const compScorecard = document.getElementById('compScore');
+const resultMsg = document.getElementById('game-message');
+const rockBtn = document.getElementById("Rock");
+const paperBtn = document.getElementById("Paper");
+const scisBtn = document.getElementById("Scissors");
+
+scisBtn.addEventListener('click', () => gameStart('Scissors'))
+rockBtn.addEventListener('click', () => gameStart('Paper'))
+paperBtn.addEventListener('click', () => gameStart('Rock'))
+
+function gameStart(playersChoice) {
+        const computerChoice = getComputerChoice();
+        playRound(computerChoice, playersChoice);
+        roundScoring();
+}
+
+function roundScoring() {
+    if (roundVictor === 'player') {
+        resultMsg.textContent = "You Won!";
+        playScorecard.textContent = `You: ${playerScore}`;
+
+    }
+    else if (roundVictor === 'computer') {
+        resultMsg.textContent = "You Lost!";
+        compScorecard.textContent = `Computer: ${computerScore}`;
+
+    }else if (roundVictor === 'tie')
+        resultMsg.textContent = "Tie!"
+}
+    //returns You Lose or You Win
     //player input should be case insensitive
 
 
@@ -32,8 +71,7 @@ const buttons = document.querySelectorAll('button');
 buttons.forEach((button) => {
     button.addEventListener('click', () => {
         playerChoice = button.id;
-        let compChoice = getComputerChoice();
-        result = playRound(compChoice, playerChoice);
+        
     });
 });
 
